09bf23aebc7c12662bb15518ac95388be59a0ffa
Rahul Saha <40941823+rahulsaha17@users.noreply.github.com>
2019-01-21 09:59:05 +0530

    Add files via upload

---
+++ email/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'nemish452@gmail.com';
+
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; 
+echo '<script>console.log($email_body)</script>';
+
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
